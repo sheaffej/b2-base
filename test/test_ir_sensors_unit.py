506ffddd5d5807a4f3168fb453b595b497537372
sheaffej <sheaffej@gmail.com>
2018-03-04 16:41:13 -0800

    more package reorg; first unit test

--- test/test_ir_sensors_unit.py
+++ test/test_ir_sensors_unit.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 import unittest
 
-import b2_base
+import b2_base.sensors_node
 
 PKG = 'b2_base'
 NAME = 'b2_ir_sensors_unittest'
@@ -12,7 +12,7 @@ class TestIRSensors(unittest.TestCase):
 
     def __init__(self, *args):
         super(TestIRSensors, self).__init__(*args)
-        self.ir_sensors = 
+        # self.ir_sensors = b2_base.sensors_node.IRSensors('ir_sensors_test')
 
     def test_volts_per_adc(self):
         # (vref, min_adc_reading, max_adc_reading, expected)
@@ -20,12 +20,14 @@ class TestIRSensors(unittest.TestCase):
             (5.0, 0, 1023, round(5.0/1023, 3)),
             (3.3, 0, 1023, round(3.3/1023, 3))
         ]
+        print()
         for vref, min_adc, max_adc, expected in tests:
             actual = round(
-                        self.ir_sensors._volts_per_adc(
+                        b2_base.sensors_node.volts_per_adc(
                             vref,
                             min_adc,
                             max_adc), 3)
+            print("Actual: {}, Expected: {}".format(actual, expected))
             self.assertEqual(actual, expected)
 
 
