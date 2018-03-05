a44d5e3cd53f1b06be450cf3c2bcb6e6845423ca
sheaffej <sheaffej@gmail.com>
2018-03-04 19:02:32 -0800

    initial sensor unit tests done

--- test/test_ir_sensors_unit.py
+++ test/test_ir_sensors_unit.py
@@ -20,6 +20,7 @@ class TestIRSensors(unittest.TestCase):
             (5.0, 0, 1023, round(5.0/1023, 3)),
             (3.3, 0, 1023, round(3.3/1023, 3))
         ]
+
         print()
         for vref, min_adc, max_adc, expected in tests:
             actual = round(
@@ -27,6 +28,62 @@ class TestIRSensors(unittest.TestCase):
                             vref,
                             min_adc,
                             max_adc), 3)
+            print("Actual: {}, Expected: {}".format(actual, expected))
+            self.assertEqual(actual, expected)
+
+    def test_volts_at_dist_cm(self):
+        # (cm, expected)
+        tests = [
+            (10, 3.649),
+            (20, 2.016),
+            (30, 1.413),
+            (50, 0.978),
+            (70, 0.822),
+            (100, 0.724),
+            (150, 0.663)
+        ]
+
+        print()
+        for cm, expected in tests:
+            actual = round(b2_base.sensors_node.volts_at_cm_distance(cm), 3)
+            print("Actual: {}, Expected: {}".format(actual, expected))
+            self.assertEqual(actual, expected)
+
+    def test_adc_at_prox_dist(self):
+        # (dist_m, v_per_adc, expected)
+        tests = [
+            (0.1, 5.0/1023, 746),
+            (0.5, 5.0/1023, 200),
+            (1.0, 5.0/1023, 148),
+            (1.5, 5.0/1023, 135),
+            (3.3, 5.0/1023, 125),
+            (4.7, 5.0/1023, 124),
+            (5.0, 5.0/1023, 123),
+            (0.1, 3.3/1023, 1131),
+            (0.5, 3.3/1023, 303),
+            (1.0, 3.3/1023, 224),
+            (1.3, 3.3/1023, 210),
+            (2.7, 3.3/1023, 192),
+            (3.0, 3.3/1023, 191),
+            (3.3, 3.3/1023, 190),
+        ]
+
+        print()
+        for dist_m, v_per_adc, expected in tests:
+            actual = b2_base.sensors_node.adc_at_proximity_dist(dist_m, v_per_adc)
+            print("Actual: {}, Expected: {}".format(actual, expected))
+            self.assertEqual(actual, expected)
+
+    def test_MCP3008stub(self):
+        # (channel, val)
+        tests = [val * 150 for val in range(8)]
+        stub = b2_base.sensors_node.MCP3008Stub()
+
+        print()
+        for idx, val in enumerate(tests):
+            stub.set_adc(idx, val)
+            actual = stub.read_adc(idx)
+            expected = val
             print("Actual: {}, Expected: {}".format(actual, expected))
             self.assertEqual(actual, expected)
 
