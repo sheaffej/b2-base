add node-level test; fixes odometry calculation

src/b2_base/base_node.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 from math import pi, sin, cos
 import threading
+import math
 
 import rospy
 from geometry_msgs.msg import Twist, Quaternion
@@ -11,8 +12,10 @@ import tf
 from roboclaw_driver.msg import SpeedCommand, Stats
 
 DEFAULT_NODE_NAME = "base_node"
-DEFAULT_CMD_TOPIC = "teleop_node/cmd_vel"
+DEFAULT_CMD_TOPIC = "base_node/cmd_vel"
 DEFAULT_ROBOCLAW_STATS_TOPIC = "roboclaw/stats"
+DEFAULT_SPEED_CMD_TOPIC = "roboclaw/speed_command"
+DEFAULT_ODOM_TOPIC = "~odom"
 DEFAULT_LOOP_HZ = 10  # hertz
 DEFAULT_WHEEL_DIST = 0.220  # meters
 DEFAULT_WHEEL_RADIUS = 0.0325  # meters
@@ -21,19 +24,11 @@ DEFAULT_MAX_QPPS = 3700
 DEFAULT_MAX_DRIVE_SECS = 1
 DEFAULT_ODOM_FRAME_ID = "world"
 DEFAULT_BASE_FRAME_ID = "base_link"
+DEFAULT_DEADMAN_SECS = 1
 
 
 class BaseNode:
-    def __init__(self, node_name):
-        self._node_name = node_name
-
-        self._x_linear_cmd = 0.0
-        self._z_angular_cmd = 0.0
-        self._cmd_vel_lock = threading.RLock()  # To serialize access to x/z command variables
-
-        self._m1_qpps_actual = 0
-        self._m2_qpps_actual = 0
-        self._stats_lock = threading.RLock()  # To serialize access to the qpps stats
+    def __init__(self):
 
         self._wheel_dist = rospy.get_param("~wheel_dist", DEFAULT_WHEEL_DIST)
         self._wheel_radius = rospy.get_param("~wheel_radius", DEFAULT_WHEEL_RADIUS)
@@ -43,11 +38,22 @@ class BaseNode:
         self._max_qpps = rospy.get_param("~max_qpps", DEFAULT_MAX_QPPS)
         self._base_frame_id = rospy.get_param("~base_frame_id", DEFAULT_BASE_FRAME_ID)
         self._world_frame_id = rospy.get_param("~odom_frame_id", DEFAULT_ODOM_FRAME_ID)
+        self._deadman_secs = rospy.get_param("~deadman_secs", DEFAULT_DEADMAN_SECS)
 
+        # Init Twist command state
+        self._x_linear_cmd = 0.0
+        self._z_angular_cmd = 0.0
+        self._last_cmd_vel_time = rospy.get_rostime()
+
+        # Init Odometry state
         self._world_x = 0.0
         self._world_y = 0.0
         self._world_theta = 0.0
-        self._last_odom_time = rospy.get_rostime()
+        # self._last_odom_time = rospy.get_rostime()
+        self._roboclaw_stats = Stats()
+
+        self._stats_lock = threading.RLock()  # To serialize access to the qpps stats
+        self._cmd_vel_lock = threading.RLock()  # To serialize access to x/z command variables
 
         # Set up the Joy message Subscriber
         rospy.Subscriber(
@@ -64,8 +70,16 @@ class BaseNode:
         )
 
         # Set up the publishers
-        self._speed_cmd_pub = rospy.Publisher('~speed_command', SpeedCommand, queue_size=1)
-        self._odom_pub = rospy.Publisher('~odom', Odometry, queue_size=1)
+        self._speed_cmd_pub = rospy.Publisher(
+            rospy.get_param('~speed_cmd_topic', DEFAULT_SPEED_CMD_TOPIC),
+            SpeedCommand,
+            queue_size=1
+        )
+        self._odom_pub = rospy.Publisher(
+            rospy.get_param('~odom_topic', DEFAULT_ODOM_TOPIC),
+            Odometry,
+            queue_size=1
+        )
         self._tf_broadcaster = tf.broadcaster.TransformBroadcaster()
 
     def run(self):
@@ -75,34 +89,56 @@ class BaseNode:
         rospy.loginfo("Running node")
         looprate = rospy.Rate(rospy.get_param("~loop_hz", DEFAULT_LOOP_HZ))
 
+        # Set initial states
+        m1_enc_prev = self._roboclaw_stats.m1_enc_val
+        m2_enc_prev = self._roboclaw_stats.m2_enc_val
+        last_odom_time = rospy.get_rostime()
+
         try:
             while not rospy.is_shutdown():
+
+                # If the last command was over 1 sec ago, stop the base
+                if (rospy.get_rostime() - self._last_cmd_vel_time).to_sec() > self._deadman_secs:
+                    self._x_linear_cmd = 0.0
+                    self._z_angular_cmd = 0.0
 
                 # ---------------------------------
                 # Calculate and send motor commands
                 # ---------------------------------
+                with self._cmd_vel_lock:
+                    x_linear_cmd = self._x_linear_cmd
+                    z_angular_cmd = self._z_angular_cmd
                 cmd = calc_create_speed_cmd(
-                    self._x_linear_cmd, self._z_angular_cmd,
+                    x_linear_cmd, z_angular_cmd,
                     self._wheel_dist, self._wheel_radius,
-                    self._ticks_per_radian, self._max_drive_secs, self._max_qpps
+                    self._ticks_per_rotation, self._max_drive_secs, self._max_qpps
                 )
                 self._speed_cmd_pub.publish(cmd)
 
                 # ----------------
                 # Publish Odometry
                 # ----------------
+                with self._stats_lock:
+                    # Calculate change in encoder readings
+                    m1_enc_diff = self._roboclaw_stats.m1_enc_val - m1_enc_prev
+                    m2_enc_diff = self._roboclaw_stats.m2_enc_val - m2_enc_prev
+
+                    m1_enc_prev = self._roboclaw_stats.m1_enc_val
+                    m2_enc_prev = self._roboclaw_stats.m2_enc_val
+
+                    nowtime = self._roboclaw_stats.header.stamp
+
                 odom = calc_create_odometry(
-                    self._m1_qpps_actual, self._m2_qpps_actual,
-                    self._ticks_per_rotation, self._wheel_dist, self._wheel_radius,
+                    m1_enc_diff, m2_enc_diff, self._ticks_per_rotation,
+                    self._wheel_dist, self._wheel_radius,
                     self._world_x, self._world_y, self._world_theta,
-                    self._last_odom_time,
+                    last_odom_time,
                     self._base_frame_id, self._world_frame_id,
-                    rospy.get_rostime()
+                    nowtime
                 )
                 self._world_x = odom.pose.pose.position.x
                 self._world_y = odom.pose.pose.position.y
-                self._world_theta = tf.transformations.euler_from_quaternion(
-                    odom.pose.pose.orientation)[2]
+                self._world_theta = yaw_from_odom_message(odom)
                 self._odom_pub.publish(odom)
 
                 # Broadcast tf transform for other nodes to use
@@ -110,11 +146,12 @@ class BaseNode:
                 self._tf_broadcaster.sendTransform(
                     (self._world_x, self._world_y, 0),
                     (quat.x, quat.y, quat.z, quat.w),
-                    rospy.Time.now(),
+                    nowtime,
                     self._base_frame_id,
                     self._world_frame_id
                 )
 
+                last_odom_time = nowtime
                 looprate.sleep()
 
         except rospy.ROSInterruptException:
@@ -129,20 +166,20 @@ class BaseNode:
         with self._cmd_vel_lock:
             self._x_linear_cmd = msg.linear.x
             self._z_angular_cmd = msg.angular.z
+            self._last_cmd_vel_time = rospy.get_rostime()
 
     def _roboclaw_stats_callback(self, stats):
         """Called by the Roboclaw Stats message subscriber
 
         Parameters:
-            :param Stats stats: Roboclaw Stas message
+            :param Stats stats: Roboclaw Stats message
         """
         with self._stats_lock:
-            self._m1_qpps_actual = stats.m1_enc_qpps
-            self._m2_qpps_actual = stats.m2_enc_qpps
+            self._roboclaw_stats = stats
 
 
 def calc_create_speed_cmd(x_linear_cmd, z_angular_cmd, wheel_dist,
-                          wheel_radius, ticks_per_radian, max_drive_secs, max_qpps):
+                          wheel_radius, ticks_per_rotation, max_drive_secs, max_qpps):
     """Calculate and send motor commands
 
     Parameters:
@@ -162,13 +199,20 @@ def calc_create_speed_cmd(x_linear_cmd, z_angular_cmd, wheel_dist,
     left_angular_v = (
         (x_linear_cmd - z_angular_cmd * (wheel_dist / 2.0)) / wheel_radius
     )
+    # print("left_angular_v: {}".format(left_angular_v))
+
+    ticks_per_radian = ticks_per_rotation / (math.pi * 2)
+    # print("ticks_per_radian: {}".format(ticks_per_radian))
 
     right_qpps_target = right_angular_v * ticks_per_radian
     left_qpps_target = left_angular_v * ticks_per_radian
+    # print("left_qpps_target: {}".format(left_qpps_target))
 
     # Clamp the target QPPS within the max_qpps
     right_qpps_target = max(-max_qpps, min(right_qpps_target, max_qpps))
     left_qpps_target = max(-max_qpps, min(left_qpps_target, max_qpps))
+    # print("right_qpps_target (after clamp): {}".format(right_qpps_target))
+    # print("left_qpps_target (after clamp): {}".format(left_qpps_target))
 
     cmd = SpeedCommand()
     cmd.m1_qpps = right_qpps_target
@@ -177,15 +221,15 @@ def calc_create_speed_cmd(x_linear_cmd, z_angular_cmd, wheel_dist,
     return cmd
 
 
-def calc_create_odometry(m1_qpps_actual, m2_qpps_actual, ticks_per_rotation,
+def calc_create_odometry(m1_enc_diff, m2_enc_diff, ticks_per_rotation,
                          wheel_dist, wheel_radius, world_x, world_y, world_theta,
-                         last_odom_time, base_frame_id, world_frame_id, now_rostime):
+                         last_odom_time, base_frame_id, world_frame_id, now_odom_time):
     '''Calculate and publish Odometry
 
     Parameters:
-        :param int m1_qpps_actual: The motor 1 QPPS reported from the Roboclaw
-        :param int m2_qpps_actual: The motor 1 QPPS reported from the Roboclaw
-        :param float ticks_per_radian: Number of encoder ticks per radion of wheel rotation
+        :param int m1_enc_diff: The motor 1 encoder change reported from the Roboclaw
+        :param int m2_enc_diff: The motor 2 encoder change reported from the Roboclaw
+        :param float ticks_per_rotation: Number of encoder ticks per radion of wheel rotation
         :param float wheel_dist: Distance between wheels (m)
         :param float wheel_radius: Wheel radius (m)
         :param float world_x: Previous world x coordinate
@@ -200,29 +244,49 @@ def calc_create_odometry(m1_qpps_actual, m2_qpps_actual, ticks_per_rotation,
         :rtype: (Odemetry, float, float, float)
     '''
     ticks_per_radian = ticks_per_rotation / (2 * pi)
+    # print("ticks_per_radian: {}".format(ticks_per_radian))
+
+    time_delta_secs = (now_odom_time - last_odom_time).to_sec()
+    last_odom_time = now_odom_time
+    # print("time_delta_secs: {}".format(time_delta_secs))
+
+    if time_delta_secs > 0:
+        m1_qpps_actual = m1_enc_diff / float(time_delta_secs)
+        m2_qpps_actual = m2_enc_diff / float(time_delta_secs)
+    else:
+        m1_qpps_actual = m2_qpps_actual = 0
+
+    # print("m1_qpps_actual: {} / {} = {}".format(m1_enc_diff, time_delta_secs, m1_qpps_actual))
+    # print("m2_qpps_actual: {} / {} = {}".format(m2_enc_diff, time_delta_secs, m2_qpps_actual))
 
     right_angular_v = m1_qpps_actual / float(ticks_per_radian)
     left_angular_v = m2_qpps_actual / float(ticks_per_radian)
+    # print("left_angular_v: {}".format(left_angular_v))
 
-    right_linear_v = right_angular_v * wheel_radius
-    left_linear_v = left_angular_v * wheel_radius
+    right_linear_v = right_angular_v * float(wheel_radius)
+    left_linear_v = left_angular_v * float(wheel_radius)
+    # print("right_linear_v: {}".format(left_linear_v))
+    # print("left_linear_v: {}".format(left_linear_v))
 
-    x_linear_v = (right_linear_v + left_linear_v) / 2
-    # y_linear_v = 0  # Because the robot is nonholonomic
-    z_angular_v = (right_linear_v - left_linear_v) / wheel_dist
+    x_linear_v = (right_linear_v + left_linear_v) / 2.0
+    y_linear_v = 0  # Because the robot is nonholonomic
+    z_angular_v = (right_linear_v - left_linear_v) / float(wheel_dist)
+    # print("x_linear_v: {}".format(x_linear_v))
+    # print("z_angular_v: {}".format(z_angular_v))
 
     # 2D rotation matrix math https://en.wikipedia.org/wiki/Rotation_matrix
-    # But since y_linear_v = 0, we don't need the second part of each equation
-    world_x_velocity = x_linear_v * cos(z_angular_v)  # - y_linear_v * sin(z_angular_v)
-    world_y_velocity = x_linear_v * sin(z_angular_v)  # + y_linear_v * cos(z_angular_v)
+    # But since y_linear_v = 0, we don't actually need the second part of each equation
+    world_x_velocity = x_linear_v * cos(world_theta) - y_linear_v * sin(world_theta)
+    world_y_velocity = x_linear_v * sin(world_theta) + y_linear_v * cos(world_theta)
     world_angular_velocity = z_angular_v
-
-    time_delta_secs = (now_rostime - last_odom_time).to_sec()
-    last_odom_time = now_rostime
+    # print("world_x_velocity: {}".format(world_x_velocity))
+    # print("world_y_velocity: {}".format(world_y_velocity))
+    # print("world_angular_velocity: {}".format(world_angular_velocity))
 
     world_x = world_x + (world_x_velocity * time_delta_secs)
     world_y = world_y + (world_y_velocity * time_delta_secs)
     world_theta = world_theta + (world_angular_velocity * time_delta_secs)
+    # print("world coordinates: ({}, {}, {})".format(world_x, world_y, world_theta))
 
     # Convert world orientation (theta) to a Quaternion for use with tf and Odometry
     quat_vals = tf.transformations.quaternion_from_euler(0, 0, world_theta)
@@ -233,11 +297,11 @@ def calc_create_odometry(m1_qpps_actual, m2_qpps_actual, ticks_per_rotation,
     quat.w = quat_vals[3]
 
     odom = Odometry()
-    odom.header.stamp = now_rostime
+    odom.header.stamp = now_odom_time
     odom.header.frame_id = world_frame_id
     odom.pose.pose.position.x = world_x
     odom.pose.pose.position.y = world_y
-    odom.pose.pose.position.z = 0
+    odom.pose.pose.position.z = 0.0
     odom.pose.pose.orientation = quat
     odom.child_frame_id = base_frame_id
     odom.twist.twist.linear.x = x_linear_v
@@ -246,8 +310,24 @@ def calc_create_odometry(m1_qpps_actual, m2_qpps_actual, ticks_per_rotation,
     return odom
 
 
+def yaw_from_odom_message(odom):
+    """Converts an Odometry message into an Euler yaw value
+    Parameters:
+        :param Odometry odom:
+
+    :rtype: float
+    """
+    return tf.transformations.euler_from_quaternion(
+        [
+            odom.pose.pose.orientation.x,
+            odom.pose.pose.orientation.y,
+            odom.pose.pose.orientation.z,
+            odom.pose.pose.orientation.w,
+        ])[2]
+
+
 if __name__ == "__main__":
     rospy.init_node(DEFAULT_NODE_NAME, log_level=rospy.DEBUG)
     node_name = rospy.get_name()
-    node = BaseNode(node_name)
+    node = BaseNode()
     node.run()

tests/test_base_unit.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import unittest
+import math
 
 import rospy
-import tf.transformations
 
 import b2_base.base_node
 
@@ -16,32 +16,38 @@ class TestBase(unittest.TestCase):
     def __init__(self, *args):
         super(TestBase, self).__init__(*args)
 
-    def test_calc_speed_command(self):
         # Robot parameters
-        wheel_dist = 0.220
-        wheel_radius = 0.0325
-        ticks_per_radian = 48 * 34
-        max_drive_secs = 1
-        max_qpps = 6000
+        self.wheel_dist = 0.220
+        self.wheel_radius = 0.0325
+        self.ticks_per_radian = 48 * 34
+        self.base_frame_id = "base_frame"
+        self.world_frame_id = "world_frame"
+        self.max_drive_secs = 1
+        self.max_qpps = 6000
+
+    def test_calc_speed_command(self):
 
         tests = [
             # (x_linear, z_angular, m1_expected, m2_expected)
             # x is m/sec, and z is radians/sec
-            (0.1, 0.0, 5022, 5022),
-            (0.0, 1.0, 5524, -5524),
-            (0.05, 0.5, 5273, -251),
-            (1.0, 0.0, max_qpps, max_qpps)
+            (0.1, 0.0, 799, 799),
+            (0.0, 0.1, 88, -88),
+            (0.05, 0.5, 839, -40),
+            (1.0, 0.0, self.max_qpps, self.max_qpps),
+            (0.0, math.pi/4, 690, -690),
+            (0.5, 0.0, 3996, 3996),
         ]
+        print()
 
         for x_linear_cmd, z_angular_cmd, m1_expected, m2_expected in tests:
             actual_cmd = b2_base.base_node.calc_create_speed_cmd(
                 x_linear_cmd,
                 z_angular_cmd,
-                wheel_dist,
-                wheel_radius,
-                ticks_per_radian,
-                max_drive_secs,
-                max_qpps
+                self.wheel_dist,
+                self.wheel_radius,
+                self.ticks_per_radian,
+                self.max_drive_secs,
+                self.max_qpps
             )
             print()
             print("### Input x_linear: {}, z_angular: {} ###".format(
@@ -51,82 +57,163 @@ class TestBase(unittest.TestCase):
             print("[m2_qpps] Actual: {}, Expected: {}".format(
                 actual_cmd.m2_qpps, m2_expected))
             print("[max_secs] Actual: {}, Expected: {}".format(
-                actual_cmd.max_secs, max_drive_secs))
+                actual_cmd.max_secs, self.max_drive_secs))
 
             self.assertEqual(round(actual_cmd.m1_qpps), m1_expected)
             self.assertEqual(round(actual_cmd.m2_qpps), m2_expected)
-            self.assertEqual(round(actual_cmd.max_secs), max_drive_secs)
+            self.assertEqual(round(actual_cmd.max_secs), self.max_drive_secs)
 
-    def test_calc_odometry(self):
+    def test_calc_odometry_single(self):
         base_node = b2_base.base_node
 
-        # Robot parameters
-        wheel_dist = 0.220
-        wheel_radius = 0.0325
-        ticks_per_radian = 48 * 34
-        base_frame_id = "base_frame"
-        world_frame_id = "world_frame"
-
-        # world_x, world_y, world_theta, last_odom_time, m1_qpps_actual, m2_qpps_actual, delta_secs
+        # world_x, world_y, world_theta, last_odom_time, m1_qpps, m2_qpps, delta_secs
         # new_world_x, new_world_y, new_world_theta, exp_linear_x, exp_linear_y, exp_angular_z
         tests = [
             (0.0, 0.0, 0.0, rospy.Time(0), 1000, 1000, 1,
                 0.125, 0.0, 0.0, 0.125, 0.0, 0.0),
             (0.0, 0.0, 0.0, rospy.Time(0), 1000, 500, 1,
-                0.09, 0.026, 0.284, 0.094, 0.0, 0.284),
+                0.094, 0.0, 0.284, 0.094, 0.0, 0.284),
+            (0.0, 0.0, 0.0, rospy.Time(0), 3700, 3700, 1,
+                0.463, 0.0, 0.0, 0.463, 0.0, 0.0),
+            (3.0, 4.0, 1.707, rospy.Time(0), 700, 2800, 0.5,
+                2.985, 4.108, 1.110, 0.219, 0.0, -1.194),
             (3.0, 4.0, 1.707, rospy.Time(12000), 700, 2800, 0.5,
-                3.04, 3.898, 1.11, 0.219, 0.0, -1.194),
+                2.985, 4.108, 1.110, 0.219, 0.0, -1.194),
         ]
 
         for (
             world_x, world_y, world_theta,
             last_odom_time,
-            m1_qpps_actual, m2_qpps_actual, delta_secs,
+            m1_qpps, m2_qpps, delta_secs,
             exp_new_world_x, exp_new_world_y, exp_new_world_theta,
             exp_linear_x, exp_linear_y, exp_angular_z
         ) in tests:
             print()
+            print("#### Input: m1: {}, m2: {}, w({}, {}, {}), secs: {}".format(
+                m1_qpps, m2_qpps, world_x, world_y, world_theta, delta_secs))
             t2 = last_odom_time + rospy.Duration(secs=delta_secs)
 
+            m1_enc_diff = m1_qpps * delta_secs
+            m2_enc_diff = m2_qpps * delta_secs
+
             odom = base_node.calc_create_odometry(
-                m1_qpps_actual,
-                m2_qpps_actual,
-                ticks_per_radian,
-                wheel_dist,
-                wheel_radius,
+                m1_enc_diff,
+                m2_enc_diff,
+                self.ticks_per_radian,
+                self.wheel_dist,
+                self.wheel_radius,
                 world_x,
                 world_y,
                 world_theta,
                 last_odom_time,
-                base_frame_id,
-                world_frame_id,
+                self.base_frame_id,
+                self.world_frame_id,
                 t2
             )
-            new_world_x = round(odom.pose.pose.position.x, 3)
-            new_world_y = round(odom.pose.pose.position.y, 3)
+            self._compare_odometry(odom, exp_new_world_x, exp_new_world_y, exp_new_world_theta,
+                                   exp_linear_x, exp_linear_y, exp_angular_z)
 
-            quat_msg = odom.pose.pose.orientation
-            euler = tf.transformations.euler_from_quaternion(
-                [quat_msg.x, quat_msg.y, quat_msg.z, quat_msg.w])
-            new_world_theta = round(euler[2], 3)
+    def test_calc_odometry_cumulative(self):
+        base_node = b2_base.base_node
 
-            new_world_linear_x = round(odom.twist.twist.linear.x, 3)
-            new_world_linear_y = round(odom.twist.twist.linear.y, 3)
-            new_world_angular_z = round(odom.twist.twist.angular.z, 3)
+        # (linear_x, angular_z, secs,
+        #  exp_world_x, exp_world_y, exp_world_theta, exp_linear_x, exp_linear_y, exp_angular_z)
+        tests = [
+            (0.5, 0.0, 2,                               # Straight for 1 meter
+                1.0, 0.0, 0.0, 0.5, 0.0, 0.0),
+            (0.0, math.pi/4, 2,                         # Turn 90 degrees
+                1.0, 0.0, math.pi/2, 0.0, 0.0, math.pi/4),
+            (0.5, 0.0, 2,                               # Straight for 1 meter
+                1.0, 1.0, math.pi/2, 0.5, 0.0, 0.0),
+            (0.0, math.pi/4, 3,                         # Turn 135 degrees
+                1.0, 1.0, -math.pi/4*3, 0.0, 0.0, math.pi/4),
+            (0.5, 0.0, 2 * (1 / math.sin(math.pi/4)),   # Straight back to origin
+                0.01, 0.01, -math.pi/4*3, 0.5, 0.0, 0.0),  # Not 0,0 because of 0.1 sec odom update
+        ]
 
-            print("Pose Actual: [{}, {}, {}], Expected: [{}, {}, {}]".format(
-                new_world_x, new_world_y, new_world_theta,
-                exp_new_world_x, exp_new_world_y, exp_new_world_theta))
-            print("Velocity Actual: [{}, {}, {}], Expected: [{}, {}, {}]".format(
-                new_world_linear_x, new_world_linear_y, new_world_angular_z,
-                exp_linear_x, exp_linear_y, exp_angular_z))
+        world_x = 0.0
+        world_y = 0.0
+        world_theta = 0.0
+        last_odom_time = rospy.Time(0)
+        delta_secs = 0.1
 
-            self.assertEqual(new_world_x, exp_new_world_x)
-            self.assertEqual(new_world_y, exp_new_world_y)
-            self.assertEqual(new_world_theta, exp_new_world_theta)
-            self.assertEqual(new_world_linear_x, exp_linear_x)
-            self.assertEqual(new_world_linear_y, exp_linear_y)
-            self.assertEqual(new_world_angular_z, exp_angular_z)
+        delta_nsecs = delta_secs * 10**9
+
+        print()
+
+        for (linear_x, angular_z, secs,
+             exp_world_x, exp_world_y, exp_world_theta,
+             exp_linear_x, exp_linear_y, exp_angular_z) in tests:
+
+            print()
+            print("CMD [x:{}, w:{}] for {} secs".format(linear_x, angular_z, secs))
+
+            for i in range(int(secs / delta_secs)):
+                t2 = last_odom_time + rospy.Duration(nsecs=delta_nsecs)
+
+                cmd = b2_base.base_node.calc_create_speed_cmd(
+                    linear_x, angular_z,
+                    self.wheel_dist, self.wheel_radius, self.ticks_per_radian,
+                    self.max_drive_secs, self.max_qpps
+                )
+
+                print("M1 QPPS: {} M2 QPPS: {}".format(cmd.m1_qpps, cmd.m2_qpps))
+
+                m1_enc_diff = cmd.m1_qpps * delta_secs
+                m2_enc_diff = cmd.m2_qpps * delta_secs
+
+                odom = base_node.calc_create_odometry(
+                    m1_enc_diff,
+                    m2_enc_diff,
+                    self.ticks_per_radian,
+                    self.wheel_dist,
+                    self.wheel_radius,
+                    world_x,
+                    world_y,
+                    world_theta,
+                    last_odom_time,
+                    self.base_frame_id,
+                    self.world_frame_id,
+                    t2
+                )
+
+                world_x = odom.pose.pose.position.x
+                world_y = odom.pose.pose.position.y
+                world_theta = b2_base.base_node.yaw_from_odom_message(odom)
+                last_odom_time = t2
+                print("x: {}, y: {}, 0: {}, t2: {}".format(
+                    world_x, world_y, world_theta, t2))
+
+            self._compare_odometry(
+                odom,
+                exp_world_x, exp_world_y, exp_world_theta,
+                exp_linear_x, exp_linear_y, exp_angular_z
+            )
+
+    def _compare_odometry(self, actual_odom, exp_new_world_x, exp_new_world_y, exp_new_world_theta,
+                          exp_linear_x, exp_linear_y, exp_angular_z):
+        new_world_x = round(actual_odom.pose.pose.position.x, 3)
+        new_world_y = round(actual_odom.pose.pose.position.y, 3)
+
+        new_world_theta = b2_base.base_node.yaw_from_odom_message(actual_odom)
+
+        new_world_linear_x = round(actual_odom.twist.twist.linear.x, 3)
+        new_world_linear_y = round(actual_odom.twist.twist.linear.y, 3)
+        new_world_angular_z = round(actual_odom.twist.twist.angular.z, 3)
+
+        print("Pose Actual: [{}, {}, {}], Expected: [{}, {}, {}]".format(
+            new_world_x, new_world_y, new_world_theta,
+            exp_new_world_x, exp_new_world_y, exp_new_world_theta))
+        print("Velocity Actual: [{}, {}, {}], Expected: [{}, {}, {}]".format(
+            new_world_linear_x, new_world_linear_y, new_world_angular_z,
+            exp_linear_x, exp_linear_y, exp_angular_z))
+
+        self.assertAlmostEqual(new_world_x, exp_new_world_x, places=2)
+        self.assertAlmostEqual(new_world_y, exp_new_world_y, places=2)
+        self.assertAlmostEqual(new_world_theta, exp_new_world_theta, places=2)
+        self.assertAlmostEqual(new_world_linear_x, exp_linear_x, places=2)
+        self.assertAlmostEqual(new_world_linear_y, exp_linear_y, places=2)
+        self.assertAlmostEqual(new_world_angular_z, exp_angular_z, places=2)
 
 
 if __name__ == "__main__":