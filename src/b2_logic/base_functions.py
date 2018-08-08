refector base odometry calculation, add unit tests, correct .coveragerc

src/b2_logic/base_functions.py
@@ -1,10 +1,9 @@
 from math import pi, sin, cos
 
-from geometry_msgs.msg import Quaternion
-from nav_msgs.msg import Odometry
-import tf.transformations
+import rospy
 
 from roboclaw_driver.msg import SpeedCommand
+from odometry_helpers import create_odometry_message, calc_world_frame_pose
 
 
 def calc_create_speed_cmd(x_linear_cmd, z_angular_cmd, wheel_dist,
@@ -50,106 +49,98 @@ def calc_create_speed_cmd(x_linear_cmd, z_angular_cmd, wheel_dist,
     return cmd
 
 
-def calc_create_odometry(m1_enc_diff, m2_enc_diff, ticks_per_rotation,
-                         wheel_dist, wheel_radius, world_x, world_y, world_theta,
-                         last_odom_time, base_frame_id, world_frame_id, now_odom_time):
-    '''Calculate and publish Odometry
+def calc_base_frame_velocity_from_encoder_diffs(
+    m1_enc_diff, m2_enc_diff,
+    ticks_per_rotation, wheel_radius, wheel_dist,
+    begin_odom_time, end_odom_time
+):
 
-    Parameters:
-        :param int m1_enc_diff: The motor 1 encoder change reported from the Roboclaw
-        :param int m2_enc_diff: The motor 2 encoder change reported from the Roboclaw
-        :param float ticks_per_rotation: Number of encoder ticks per radion of wheel rotation
-        :param float wheel_dist: Distance between wheels (m)
-        :param float wheel_radius: Wheel radius (m)
-        :param float world_x: Previous world x coordinate
-        :param float world_y: Previous world y cooridate
-        :param float world_theta: Previous angular orientation in the world frame (radians)
-        :param datetime.Time last_odom_time: Last time Odometry was calculated
-        :param str base_frame_id: Frame ID of the base
-        :param str world_frame_id: Frame ID of the world
-
-    Returns: Calculated Odometry and new world coordinates
-        as a tuple (Odometry, world_x, world_y, world_theta)
-        :rtype: (Odemetry, float, float, float)
-    '''
-    ticks_per_radian = ticks_per_rotation / (2 * pi)
-    # print("ticks_per_radian: {}".format(ticks_per_radian))
-
-    time_delta_secs = (now_odom_time - last_odom_time).to_sec()
-    last_odom_time = now_odom_time
+    time_delta_secs = (end_odom_time - begin_odom_time).to_sec()
     # print("time_delta_secs: {}".format(time_delta_secs))
 
+    m1_qpps_actual, m2_qpps_actual = _calc_qpps(m1_enc_diff, m2_enc_diff, time_delta_secs)
+    # print("m1_qpps_actual: {} / {} = {}".format(m1_enc_diff, time_delta_secs, m1_qpps_actual))
+    # print("m2_qpps_actual: {} / {} = {}".format(m2_enc_diff, time_delta_secs, m2_qpps_actual))
+
+    left_angular_v, right_angular_v = _calc_wheel_angular_velocity(
+        m1_qpps_actual, m2_qpps_actual, ticks_per_rotation)
+    # print("left_angular_v: {}".format(left_angular_v))
+
+    left_linear_v, right_linear_v = _calc_wheel_linear_velocity(
+        left_angular_v, right_angular_v, wheel_radius)
+    # print("right_linear_v: {}".format(left_linear_v))
+    # print("left_linear_v: {}".format(left_linear_v))
+
+    x_linear_v, y_linear_v, z_angular_v = _calc_base_frame_velocity(
+        left_linear_v, right_linear_v, wheel_dist)
+    return x_linear_v, y_linear_v, z_angular_v
+
+
+def calc_odometry_from_base_velocity(
+    x_linear_v, y_linear_v, z_angular_v,
+    world_x, world_y, world_theta,
+    time_delta_secs, odom_time,
+    base_frame_id, world_frame_id
+):
+    world_x_velocity, world_y_velocity, world_angular_velocity = _calc_world_frame_velocity(
+        x_linear_v, y_linear_v, z_angular_v, world_theta)
+    # print("world_x_velocity: {}".format(world_x_velocity))
+    # print("world_y_velocity: {}".format(world_y_velocity))
+    # print("world_angular_velocity: {}".format(world_angular_velocity))
+
+    world_x, world_y, world_theta = calc_world_frame_pose(
+        world_x_velocity, world_y_velocity, world_angular_velocity,
+        world_x, world_y, world_theta, time_delta_secs)
+    # print("world coordinates: ({}, {}, {})".format(world_x, world_y, world_theta))
+
+    odom = create_odometry_message(
+        world_x, world_y, world_theta,
+        x_linear_v, z_angular_v,
+        odom_time,
+        base_frame_id, world_frame_id
+    )
+    return odom
+
+
+def _calc_qpps(m1_enc_diff, m2_enc_diff, time_delta_secs):
     if time_delta_secs > 0:
         m1_qpps_actual = m1_enc_diff / float(time_delta_secs)
         m2_qpps_actual = m2_enc_diff / float(time_delta_secs)
     else:
         m1_qpps_actual = m2_qpps_actual = 0
+    return (m1_qpps_actual, m2_qpps_actual)
 
-    # print("m1_qpps_actual: {} / {} = {}".format(m1_enc_diff, time_delta_secs, m1_qpps_actual))
-    # print("m2_qpps_actual: {} / {} = {}".format(m2_enc_diff, time_delta_secs, m2_qpps_actual))
 
-    right_angular_v = m1_qpps_actual / float(ticks_per_radian)
-    left_angular_v = m2_qpps_actual / float(ticks_per_radian)
-    # print("left_angular_v: {}".format(left_angular_v))
+def _calc_wheel_angular_velocity(m1_qpps, m2_qpps, ticks_per_rotation):
+    ticks_per_radian = ticks_per_rotation / (2 * pi)
+    if ticks_per_radian <= 0.0:
+        rospy.logerr("ticks_per_rotation cannot be <= 0.0 (val: {})".format(ticks_per_rotation))
+        return (0.0, 0.0)
+    right_angular_v = m1_qpps / float(ticks_per_radian)
+    left_angular_v = m2_qpps / float(ticks_per_radian)
+    return (left_angular_v, right_angular_v)
 
+
+def _calc_wheel_linear_velocity(left_angular_v, right_angular_v, wheel_radius):
+    if wheel_radius <= 0.0:
+        wheel_radius = 0.0  # Set it to zero so the linear_v calculates to 0.0
+        rospy.logwarn("wheel_radius cannot be <- 0.0 (val: {})".format(wheel_radius))
     right_linear_v = right_angular_v * float(wheel_radius)
     left_linear_v = left_angular_v * float(wheel_radius)
-    # print("right_linear_v: {}".format(left_linear_v))
-    # print("left_linear_v: {}".format(left_linear_v))
+    return (left_linear_v, right_linear_v)
 
+
+def _calc_base_frame_velocity(left_linear_v, right_linear_v, wheel_dist):
     x_linear_v = (right_linear_v + left_linear_v) / 2.0
     y_linear_v = 0  # Because the robot is nonholonomic
     z_angular_v = (right_linear_v - left_linear_v) / float(wheel_dist)
-    # print("x_linear_v: {}".format(x_linear_v))
-    # print("z_angular_v: {}".format(z_angular_v))
+    return (x_linear_v, y_linear_v, z_angular_v)
 
+
+def _calc_world_frame_velocity(x_linear_v, y_linear_v, z_angular_v, world_theta):
     # 2D rotation matrix math https://en.wikipedia.org/wiki/Rotation_matrix
     # But since y_linear_v = 0, we don't actually need the second part of each equation
     world_x_velocity = x_linear_v * cos(world_theta) - y_linear_v * sin(world_theta)
     world_y_velocity = x_linear_v * sin(world_theta) + y_linear_v * cos(world_theta)
     world_angular_velocity = z_angular_v
-    # print("world_x_velocity: {}".format(world_x_velocity))
-    # print("world_y_velocity: {}".format(world_y_velocity))
-    # print("world_angular_velocity: {}".format(world_angular_velocity))
-
-    world_x = world_x + (world_x_velocity * time_delta_secs)
-    world_y = world_y + (world_y_velocity * time_delta_secs)
-    world_theta = world_theta + (world_angular_velocity * time_delta_secs)
-    # print("world coordinates: ({}, {}, {})".format(world_x, world_y, world_theta))
-
-    # Convert world orientation (theta) to a Quaternion for use with tf and Odometry
-    quat_vals = tf.transformations.quaternion_from_euler(0, 0, world_theta)
-    quat = Quaternion()
-    quat.x = quat_vals[0]
-    quat.y = quat_vals[1]
-    quat.z = quat_vals[2]
-    quat.w = quat_vals[3]
-
-    odom = Odometry()
-    odom.header.stamp = now_odom_time
-    odom.header.frame_id = world_frame_id
-    odom.pose.pose.position.x = world_x
-    odom.pose.pose.position.y = world_y
-    odom.pose.pose.position.z = 0.0
-    odom.pose.pose.orientation = quat
-    odom.child_frame_id = base_frame_id
-    odom.twist.twist.linear.x = x_linear_v
-    odom.twist.twist.linear.y = 0
-    odom.twist.twist.angular.z = z_angular_v
-    return odom
-
-
-# def yaw_from_odom_message(odom):
-#     """Converts an Odometry message into an Euler yaw value
-#     Parameters:
-#         :param Odometry odom:
-
-#     :rtype: float
-#     """
-#     return tf.transformations.euler_from_quaternion(
-#         [
-#             odom.pose.pose.orientation.x,
-#             odom.pose.pose.orientation.y,
-#             odom.pose.pose.orientation.z,
-#             odom.pose.pose.orientation.w,
-#         ])[2]
+    return (world_x_velocity, world_y_velocity, world_angular_velocity)

src/b2_logic/odometry_helpers.py
@@ -1,16 +1,13 @@
+from math import pi
+from numpy import sign
+
 import tf.transformations
+from geometry_msgs.msg import Quaternion
+from nav_msgs.msg import Odometry
 
 
 def heading_from_odometry(odom):
-    """ Extracts the heading (i.e. yaw) from an Odometry message
-
-    Parameters:
-        :param Odometry odom: The Odometry message
-
-    Returns: The heading (yaw)
-        :rtype: float
-    """
-    return tf.transformations.euler_from_quaternion(odom.pose.pose.orientation)[2]
+    return yaw_from_odom_message(odom)
 
 
 def yaw_from_odom_message(odom):
@@ -27,3 +24,50 @@ def yaw_from_odom_message(odom):
             odom.pose.pose.orientation.z,
             odom.pose.pose.orientation.w,
         ])[2]
+
+
+def create_odometry_message(world_x, world_y, world_theta,
+                            x_linear_v, z_angular_v, odom_time,
+                            base_frame_id, world_frame_id):
+    # Convert world orientation (theta) to a Quaternion for use with tf and Odometry
+    quat_vals = tf.transformations.quaternion_from_euler(0, 0, world_theta)
+    quat = Quaternion()
+    quat.x = quat_vals[0]
+    quat.y = quat_vals[1]
+    quat.z = quat_vals[2]
+    quat.w = quat_vals[3]
+
+    odom = Odometry()
+    odom.header.stamp = odom_time
+    odom.header.frame_id = world_frame_id
+    odom.pose.pose.position.x = world_x
+    odom.pose.pose.position.y = world_y
+    odom.pose.pose.position.z = 0.0
+    odom.pose.pose.orientation = quat
+    odom.child_frame_id = base_frame_id
+    odom.twist.twist.linear.x = x_linear_v
+    odom.twist.twist.linear.y = 0
+    odom.twist.twist.angular.z = z_angular_v
+    return odom
+
+
+def calc_world_frame_pose(world_x_velocity, world_y_velocity, world_angular_velocity,
+                          begin_world_x, begin_world_y, begin_world_theta, time_delta_secs):
+    """Given world velocity vectors, movement duration, and beginning world coordinates
+    calculate the new world coordinates.
+    """
+    new_world_x = begin_world_x + (world_x_velocity * time_delta_secs)
+    new_world_y = begin_world_y + (world_y_velocity * time_delta_secs)
+    new_world_theta = begin_world_theta + (world_angular_velocity * time_delta_secs)
+    new_world_theta = normalize_theta(new_world_theta)
+    return (new_world_x, new_world_y, new_world_theta)
+
+
+def normalize_theta(theta):
+    """Convert the result of adding or subtracting angular movements to a theta
+    that falls within the range of 0.0 >= theta => 2pi, where theta is always positive.
+    """
+    norm_theta = theta % (pi * 2)
+    if norm_theta < 0.0:
+        norm_theta = (pi * 2) + norm_theta
+    return norm_theta