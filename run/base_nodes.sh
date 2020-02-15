b2e152d3c6197f82c2f184eb667575f9dd3987f8
sheaffej <sheaffej@gmail.com>
2020-02-15 12:48:47 -0800

    on-robot cleanup of launch files/scripts

--- run/base_nodes.sh
+++ run/base_nodes.sh
@@ -5,14 +5,11 @@ CONTAINER_NAME="b2_base_nodes"
 
 [ -z "$ROS_MASTER_URI" ] && echo "Please set ROS_MASTER_URI env" && exit 1
 
-# MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# REPOS_DIR=$MYDIR/../..  # Directory containing the cloned git repos
-# CODE_MOUNT="/workspaces"
-
+echo "Starting container: ${CONTAINER_NAME}"
 docker run -d --rm \
 --name ${CONTAINER_NAME} \
 --privileged \
 --net host \
 --env DISPLAY \
 --env ROS_MASTER_URI \
-${DOCKER_IMAGE} roslaunch b2_base b2-base.launch
+${DOCKER_IMAGE} roslaunch --screen b2_base b2-base.launch
