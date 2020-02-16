8c8fbeb8e5934f9038e0b9d9f487adb889c9b7fd
sheaffej <sheaffej@gmail.com>
2020-02-15 17:03:06 -0800

    add test_mode to start script

--- run/base_nodes.sh
+++ run/base_nodes.sh
@@ -5,6 +5,18 @@ CONTAINER_NAME="b2_base_nodes"
 
 [ -z "$ROS_MASTER_URI" ] && echo "Please set ROS_MASTER_URI env" && exit 1
 
+while [ $# -gt 0 ]; do
+    case $1 in
+        "test")
+            TEST="test_mode:=true"
+            echo "Running in test mode"
+            ;;
+        *)
+            echo "Unknown argument" $1
+    esac
+    shift
+done
+
 echo "Starting container: ${CONTAINER_NAME}"
 docker run -d --rm \
 --name ${CONTAINER_NAME} \
@@ -12,4 +24,4 @@ docker run -d --rm \
 --net host \
 --env DISPLAY \
 --env ROS_MASTER_URI \
-${DOCKER_IMAGE} roslaunch --screen b2_base b2-base.launch
+${DOCKER_IMAGE} roslaunch --screen b2_base b2-base.launch ${TEST}
