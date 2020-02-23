153377adaa2dd1ac9b9c5be94d1e42e54dec831c
sheaffej <sheaffej@gmail.com>
2020-02-22 19:32:04 -0800

    add dev option to run/base_nodes.sh

--- run/base_nodes.sh
+++ run/base_nodes.sh
@@ -11,6 +11,9 @@ while [ $# -gt 0 ]; do
             TEST="test_mode:=true"
             echo "Running in test mode"
             ;;
+        "dev")
+            VOLUMES="--mount type=bind,source=$PROJ_DIR/b2-base,target=$CODE_MOUNT/b2-base"
+            VOLUMES="$VOLUMES --mount type=bind,source=$PROJ_DIR/roboclaw_driver,target=$CODE_MOUNT/roboclaw_driver"
         *)
             echo "Unknown argument" $1
     esac
@@ -24,4 +27,5 @@ docker run -d --rm \
 --net host \
 --env DISPLAY \
 --env ROS_MASTER_URI \
+$VOLUMES \
 ${DOCKER_IMAGE} roslaunch --screen b2_base b2-base.launch ${TEST}
