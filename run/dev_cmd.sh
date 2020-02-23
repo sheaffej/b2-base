c696583024df374b368e08e651d79b584b8cc0c2
sheaffej <sheaffej@gmail.com>
2020-02-22 19:27:41 -0800

    update roboclaw launch file

--- run/dev_cmd.sh
+++ run/dev_cmd.sh
@@ -5,7 +5,7 @@ DOCKER_IMAGE="sheaffej/b2-base"
 [ -z "$ROS_MASTER_URI" ] && echo "Please set ROS_MASTER_URI env" && exit 1
 
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PROJ_DIR=$MYDIR/..  # Directory containing the cloned git repos
+PROJ_DIR=$MYDIR/../..  # Directory containing the cloned git repos
 CODE_MOUNT="/workspaces"
 
 docker run -it --rm \
@@ -13,5 +13,6 @@ docker run -it --rm \
 --net host \
 --env DISPLAY \
 --env ROS_MASTER_URI \
---mount type=bind,source=$PROJ_DIR,target=$CODE_MOUNT/b2-base \
+--mount type=bind,source=$PROJ_DIR/b2-base,target=$CODE_MOUNT/b2-base \
+--mount type=bind,source=$PROJ_DIR/roboclaw_driver,target=$CODE_MOUNT/roboclaw_driver \
 ${DOCKER_IMAGE} $@
