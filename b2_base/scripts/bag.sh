45247dd1a4a3f29b78252ba1117f04a8e709c169
sheaffej <sheaffej@gmail.com>
2020-08-03 16:59:26 -0700

    update bag.sh for pointcloud

--- b2_base/scripts/bag.sh
+++ b2_base/scripts/bag.sh
@@ -1,2 +1,3 @@
 #rosbag record -o $HOME/Downloads/b2_downstairs.bag /camera/scan /odom /camera/depth/camera_info /camera/depth/image_rect_raw /cmd_vel /tf /tf_static
-rosbag record -o $HOME/Downloads/b2_downstairs.bag /camera/scan /odom  /tf /tf_static
+#rosbag record -o $HOME/Downloads/b2_downstairs.bag /camera/scan /odom /imu /tf /tf_static
+rosbag record -j -o $HOME/Downloads/b2_downstairs.bag /camera/depth/camera_info /camera/depth/color/points /odom /imu /tf /tf_static
